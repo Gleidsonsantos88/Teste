8d59090b706df0814299376ab5b7007945c49e61
Gleidson Jeferson Souza Santos <gleidson.santos@oleconsignado.com.br>
2021-04-08 10:30:01 -0300

    Add project files.

---
+++ ProdapWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProdapWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
